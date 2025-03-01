890e0f7de5df3ba35c750aa7e75aceb3429fded0
Quamin Hua <quaminhua56@gmail.com>
2025-02-28 19:19:00 -0800

    Add ability to add books to library

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const bookContainer = document.querySelector("div.book-container");
+const newBookButton = document.querySelector("#new-book-btn");
+const newBookModal = document.querySelector("#new-book-modal");
+const titleInput = document.querySelector("#title");
+const authorInput = document.querySelector("#author");
+const pagesInput = document.querySelector("#pages");
+const readInput = document.querySelector("#read");
+const cancelButton = document.querySelector("#cancel-btn");
+const submitButton = document.querySelector("#submit-btn");
 
 const library = [
     {
@@ -15,12 +23,14 @@ function Book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
 }
-Book.prototype.toggleRead = function() {
+Book.prototype.toggleRead = function () {
     this.read = !this.read;
 }
 
-function addBookToLibrary(book) {
+function addBookToLibrary(title, author, pages, read) {
+    const book = new Book(title, author, pages, read);
     library.push(book);
+    renderLibrary();
 }
 
 function renderLibrary() {
@@ -30,21 +40,50 @@ function renderLibrary() {
         const book = library[i];
 
         const bookCard = document.createElement('div');
-        const bookTitle = document.createElement('h2');
+        const title = document.createElement('h2');
         const author = document.createElement('h3');
         const numPages = document.createElement('p');
+        const toggleReadBtn = document.createElement('button');
+        const deleteBtn = document.createElement('button');
 
         bookCard.id = i;
         bookCard.classList.add("book-card");
-        bookTitle.textContent = book.title;
+        title.textContent = book.title;
         author.textContent = book.author;
         numPages.textContent = book.pages;
+        toggleReadBtn.textContent = "Read";
+        deleteBtn.textContent = "Delete";
 
-        bookCard.appendChild(bookTitle);
+        bookCard.appendChild(title);
         bookCard.appendChild(author);
         bookCard.appendChild(numPages);
+        bookCard.appendChild(toggleReadBtn);
+        bookCard.appendChild(deleteBtn);
         bookContainer.appendChild(bookCard);
     }
 }
+
+function resetInputs() {
+    titleInput.value = "";
+    authorInput.value = "";
+    pagesInput.value = "";
+    readInput.checked = false;
+}
+
+newBookButton.addEventListener("click", (event) => {
+    newBookModal.showModal();
+});
+
+submitButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    const title = titleInput.value;
+    const author = authorInput.value;
+    const pages = pagesInput.value;
+    const read = readInput.checked;
+    console.log(title, author, pages, read);
+    addBookToLibrary(title, author, pages, read);
+    resetInputs();
+    newBookModal.close();
+});
 
 renderLibrary();
